188ec76d172f63f98325f6022430a86cc407c810
박성현 <qkr970504@gmail.com>
2023-11-22 18:11:28 +0900

    MemberService CRUD 구현

--- optional/src/main/java/org/example/service/MemberService.java
+++ optional/src/main/java/org/example/service/MemberService.java
@@ -12,17 +12,41 @@ public class MemberService {
 
     private final MemberRepository memberRepository = new MemoryMemberRepository();
 
+    private static final String DATA_CHECK_ERROR = "데이터를 확인해주세요.";
+    private static final String DATA_NOT_FOUND = "저장된 회원이 없습니다.";
+
     public Long saveMember(SaveDto saveDto) {
         Optional<SaveDto> saveMember = Optional.ofNullable(saveDto);
-        return memberRepository.save(saveMember.orElseThrow(() -> new IllegalArgumentException("입력값 확인 필요")));
+        return memberRepository.save(saveMember.orElseThrow(() -> new IllegalArgumentException(DATA_CHECK_ERROR)));
     }
 
     public ResponseDto findById(Long id) {
-        Optional<ResponseDto> findMember = Optional.ofNullable(memberRepository.findById(id));
-        return findMember.orElseThrow(() -> new IllegalArgumentException("저장된 회원 정보 없음"));
+        if (id == null) {
+            throw new IllegalArgumentException(DATA_CHECK_ERROR);
+        }
+        return memberRepository.findById(id);
     }
 
     public List<ResponseDto> findAll() {
-        return memberRepository.findAll();
+        List<ResponseDto> allMember = memberRepository.findAll();
+        if (allMember.isEmpty()) {
+            throw new IllegalArgumentException(DATA_NOT_FOUND);
+        }
+
+        return allMember;
+    }
+
+    public void update(UpdateDto updateDto, Long id) {
+        if (updateDto == null || id == null) {
+            throw new IllegalArgumentException(DATA_CHECK_ERROR);
+        }
+        memberRepository.update(updateDto, id);
+    }
+
+    public void delete(Long id) {
+        if (id == null) {
+            throw new IllegalArgumentException(DATA_CHECK_ERROR);
+        }
+        memberRepository.delete(id);
     }
 }
