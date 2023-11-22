9ff1d28fe98d48757f0c3058924082895c24f54e
박성현 <qkr970504@gmail.com>
2023-11-22 14:05:09 +0900

    Member 필수값 설정

--- optional/src/main/java/org/example/member/Member.java
+++ optional/src/main/java/org/example/member/Member.java
@@ -1,13 +1,16 @@
 package org.example.member;
 
+import com.sun.istack.internal.NotNull;
 import lombok.*;
 
 @Getter
 @Builder
-public class Member {
+class Member {
 
     private Long id;
+    @NotNull
     private String memberId;
+    @NotNull
     private String pwd;
     private String email;
 }
